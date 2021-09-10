36641ef8d67f3b50aa595698b56a72a146455947
Nikita12112002 <guptaniki488@gmail.com>
2021-09-10 12:17:51 +0530

    commit

--- Day9/index.js
+++ Day9/index.js
@@ -1,1 +1,16 @@
 // SENDING STATIC FILES
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use('/' , (req , res)=>
+{
+  // res.sendFile(path.join(__dirname , "public/Hello.txt"),'test.txt')
+  // res.sendFile(path.join(__dirname , "public/text.json"))
+    res.download(path.join(__dirname , "public/image.jpg"),"download.jpg")
+   //  res.download(path.join(__dirname , "public/text.json"))
+  
+});
+
+app.listen(5000);
